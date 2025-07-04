edda191e8f0a4406ab5029d8f80edc3f0361f538
decodewithParth <137977825+decodewithParth@users.noreply.github.com>
2025-07-04 13:39:11 +0530

    reversed the timeline content

--- src/component/aceternity/timeline.tsx
+++ src/component/aceternity/timeline.tsx
@@ -42,8 +42,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           Changelog from my journey
         </h2>
         <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-          I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
-          a timeline of my journey.
+          Just a sneak peek of our journey.
         </p>
       </div>
 
