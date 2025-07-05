725b0098c3fc7b235c13461b5f96171ffa860798
decodewithParth <137977825+decodewithParth@users.noreply.github.com>
2025-07-05 22:33:00 +0530

    just commented the devIndicators in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators:{
-    buildActivity: false,
-  }
+  // devIndicators:{
+  //   buildActivity: false,
+  // }
 };
 
 export default nextConfig;
