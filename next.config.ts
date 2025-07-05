beac57795827e6339f6679252c3ba8ca906bf619
decodewithParth <137977825+decodewithParth@users.noreply.github.com>
2025-07-05 22:38:41 +0530

    created the ignoreDuringBuilds to true

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // devIndicators:{
-  //   buildActivity: false,
-  // }
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
